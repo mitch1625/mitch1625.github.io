942817456f89d0759bf8f609809f56cc7263b3d5
eric <ericmitchell1625@hotmail.com>
2024-03-15 19:49:51 -0400

    settings

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Projects from "../components/Projects"
 import Experience from "../components/Experience"
 import Footer from "../components/Footer"
 const HomePage = () => {
+    console.log('hello')
     return (
     <>
     <AboutMe/>
